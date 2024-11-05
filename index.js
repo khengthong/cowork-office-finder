7e0b8e831109b8fa7a74daaa60b53cff2f749074
ktpoh <khengthong@gmail.com>
2024-11-05 18:26:56 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post('/route', async (req, res) => {
+  const { origin, destination, mode } = req.body;
+
+  const googleMapsKey = 'AIzaSyC_kv5ej-7iDRWjnyWV2CzpLaEwDCXUkDA';
+  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=${mode}&key=${googleMapsKey}`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+    console.log (response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching directions');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+
+
+/* old codes
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+}); */
